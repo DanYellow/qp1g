6483867cdb454cff480b24d56fded07bf6816c8e
DanYellow <jeanlouis.danielo@sfr.fr>
2017-09-08 10:28:56 +0200

    Splits into dedicated Answer component

---
+++ src/components/Answer.vue
@@ -0,0 +1,71 @@
+<template>
+  <li class="answer">
+    <input 
+      type="radio"
+      @change="handleChange"
+      :value="answer.id"
+      :id="answer.id"
+      name="answer"
+      v-model="response">
+    <label :for="answer.id">
+      <span>{{ answer.title }} | {{ answer.id }}</span>
+    </label>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'answer',
+  props: ['answer'],
+  data () {
+    return {
+      response: null
+    }
+  },
+  methods: {
+    handleChange () {
+      console.log('ed', this.response)
+      this.$emit('answered', this.response)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="postcss">
+  .answer {
+    box-sizing: border-box;
+    flex: 1 1 calc(50% - 6px);
+    height: 50px;
+    border: 1px solid #d1d1d1;
+
+    margin: 3px;
+
+    &:hover {
+      background-color: #f3f3f3;
+      color: black;
+      font-weight: bold;
+      border-color: black;
+    }
+
+    & input[type="radio"] {
+      display: none;
+    }
+
+    & label {
+      height: 100%;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    & input[type="radio"]:active + label {
+      color: red;
+      @nest li & {
+        background-color: red;
+      }
+    }
+  }
+
+</style>
--- src/components/Question.vue
+++ src/components/Question.vue
@@ -2,24 +2,27 @@
   <section class="question">
     <div v-if="Object.keys(question).length > 0 && isCorrect === null">
       <h1 v-text="question.question"></h1>
-      <form @submit.prevent="handleSubmit">
+      <form>
         <ul class="answers">
-          <li v-for="answer in question.answers" :key="answer.id">
-            <input 
-              type="radio"
-              @change="handleSubmit"
-              :value="answer.id"
-              :id="answer.id"
-              v-model="response">
-            <label :for="answer.id">
-              <span>{{ answer.title }}</span>
-            </label>
-          </li>
+          <answer
+            @answered="handleAnswer"
+            v-for="answer in question.answers" 
+            :key="answer.id" 
+            :answer="answer"></answer>
         </ul>
       </form>
     </div>
       <div class="result">
-        <p v-if="isCorrect === true">Correct</p>
+        <section >
+          <svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="33.557px" height="27.118px" viewBox="0 0 33.557 27.118" enable-background="new 0 0 33.557 27.118" xml:space="preserve">
+<path class="path"  display="none" fill="#010202" d="M-246.798-169.002l-11.285,11.566l-5.286-5.01l-3.714,3.716l9,8.728l15-15.285
+	L-246.798-169.002z"/>
+<polyline fill="none" stroke="#010202" stroke-width="5" stroke-miterlimit="10" points="6.261,12.759 13.708,19.331 27.205,6.168 
+	"/>
+</svg>
+          <p>Correct</p>
+        </section>
         <p v-if="isCorrect === false">False</p>
       </div>
   </section>
@@ -28,12 +31,14 @@
 <script>
 import axios from 'axios'
 
+import answer from './Answer'
+
 export default {
   name: 'question',
+  components: { answer },
   data () {
     return {
       question: {},
-      response: null,
       isCorrect: null,
       answers: []
     }
@@ -42,9 +47,9 @@ export default {
     this.fetchAnswer()
   },
   methods: {
-    handleSubmit (e) {
+    handleAnswer (response) {
       const questionAnswer = this.question.response_id
-      if (questionAnswer === this.response) {
+      if (questionAnswer === response) {
         this.isCorrect = true
       } else {
         this.isCorrect = false
@@ -62,7 +67,6 @@ export default {
 
       this.question = randQuestion
       this.isCorrect = null
-      this.response = null
     }
   }
 }
@@ -87,39 +91,20 @@ export default {
     flex-wrap: wrap;
   }
 
-  .answers li {
-    box-sizing: border-box;
-    flex: 1 1 calc(50% - 6px);
-    height: 50px;
-    border: 1px solid #d1d1d1;
+  .path {
+  stroke-dasharray: 1000;
+  stroke-dashoffset: 1000;
+  animation: dash 10s linear forwards;
+}
 
-    margin: 3px;
-
-    &:hover {
-      background-color: #cecece;
-      color: white;
-      font-weight: bold;
-    }
-
-    & input[type="radio"] {
-      display: none;
-    }
-
-    & label {
-      height: 100%;
-      width: 100%;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-
-    & input[type="radio"]:active + label {
-      color: red;
-      @nest li & {
-        background-color: red;
-      }
-    }
+@keyframes dash {
+  from {
+    stroke-dashoffset: 1000;
   }
+  to {
+    stroke-dashoffset: 0;
+  }
+}
 
 </style>
 font: 25px Georgia;
